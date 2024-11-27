bde9f68e9036a2a5925290f14f03e750d6b88554
Rachit Mittal <rachit@brenolabs.com>
2024-11-27 16:38:56 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -16,5 +16,53 @@ module.exports = {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  bootstrap({ strapi }) {
+    
+    let { Server } = require("socket.io");
+    var io = new Server(strapi.server.httpServer, {
+      cors: { // cors setup
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+        allowedHeaders: ["my-custom-header"],
+        credentials: true,
+      },
+    });
+    io.on("connection", (socket) => {
+      //Listening for a connection from the frontend
+      console.log("A user connected : ", socket.id);
+      
+      // socket.on("join", ({ username }) => { // Listening for a join connection
+      //   console.log("user connected");
+      //   console.log("username is ", username);
+      //   if (username) {
+      //     socket.join("group"); // Adding the user to the group
+      //     socket.emit("welcome", { // Sending a welcome message to the User
+      //       user: "bot",
+      //       text: `${username}, Welcome to the group chat`,
+      //       userData: username,
+      //     });
+      //   } else {
+      //     console.log("An error occurred");
+      //   }
+      // });
+      socket.on("sendMessage", async (data) => { // Listening for a sendMessage connection
+        let strapiData = { // Generating the message data to be stored in Strapi
+          data: {
+            user: data.user,
+            message: data.message,
+          },
+        };
+        var axios = require("axios");
+        await axios
+          .post("http://localhost:1337/api/messages", strapiData)//Storing the messages in Strapi
+          .then((e) => {
+            socket.emit("message", {//Sending the message to the group
+              user: data.username,
+              text: data.message,
+            });
+          })
+          .catch((e) => console.log("error", e.message));
+      });
+    });
+  },
 };
